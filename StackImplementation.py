0de4ee0647fbde37b472ff25d5b73aeb409e2228
omgparth <parththakur801@gmail.com>
2026-06-06 19:36:23 +0100

    completed

--- StackImplementation.py
+++ StackImplementation.py
@@ -10,4 +10,20 @@ class Linter:
             self.stack.pop()
 
         matching_braces = {"(":")", "[": "]", "{":"}"} #å dict
+
+        for char in text:
+            if char in matching_braces.keys():
+                self.stack.push(char)
+            elif char in matching_braces.values():
+                if not self.stack.read():
+                    return char + "does not have opening brace"
+                else: 
+                    popped_opening_brace = self.stack.pop()
+
+                    if char != matching_braces.get(popped_opening_brace):
+                        return char + "has mismathed opening brace"
+                    
+        if self.stack.read():
+            return self.stack.read() + "does not have closing brace"
         
+        return True #given no errors
