51bda632409614b1d09a6cfb2edc49f79a299551
omgparth <parththakur801@gmail.com>
2026-06-07 21:00:23 +0100

    Numpyprac

---
+++ NumpyFundamentals.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#from a list
+
+prices = np.array([100, 102, 109, 101, 103])
+print(f"prices: {prices}")
+print(f"Type: {type(prices)}")
+
+##using functions to create common array
+zeros = np.zeros(5)
+ones = np.ones(5)
+print(f"Zeros: {zeros}")
